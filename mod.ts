59cc7e54a4afb24f1a5394d30c3fb96f3c5a3b05
urielch <uriel.chemouni@gmail.com>
2024-07-18 09:54:57 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,39 +10,56 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/tb)
  * 
- * ## import_map.json
+ * ## install packahe
  * 
- * For a transparent usage:
- * 
- * ```json
- * {
- *   "imports": {
- *     "@preact-icons/common": "jsr:@preact-icons/common@^1.0.10",
- *     "preact": "npm:preact@10.22.1",
- *     "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *     "preact/hooks": "npm:preact@10.22.1/hooks",
- *     "react-icons/tb": "jsr:@preact-icons/tb@^1.0.11/mod.ts",
- *     "react-icons/tb/": "jsr:@preact-icons/tb@^1.0.11/ico/",
- *   }
- * }
+ * ```bash
+ * deno add @preact-icons/tb
+ * dnpx jsr add @preact-icons/tb
+ * pnpm dlx jsr add @preact-icons/tb
+ * bunx jsr add @preact-icons/tb
  * ```
  * 
- * ## Import an icon without import_map by and afer loading all icons from the lib tb
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
  * 
- * ```ts
- * import { TbAccessibleFilled } from "jsr:preact-icons/tb@1.0.11/mod.ts"
+ * 
+ * # Tabler Icons icons for preact
+ * 
+ * [![JSR](https://jsr.io/badges/@preact-icons/tb)](https://jsr.io/@preact-icons/tb)
+ * 
+ * **License** [MIT](https://opensource.org/licenses/MIT)
+ * 
+ * **Project** [https://github.com/tabler/tabler-icons](https://github.com/tabler/tabler-icons)
+ * 
+ * [See available icons here](https://react-icons.deno.dev/tb)
+ * 
+ * ## install packahe
+ * 
+ * ```bash
+ * deno add @preact-icons/tb
+ * dnpx jsr add @preact-icons/tb
+ * pnpm dlx jsr add @preact-icons/tb
+ * bunx jsr add @preact-icons/tb
  * ```
  * 
- * ## import_map import an icon from all icons
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
+ * 
+ * 
+ * ## import an icon from all icons
  * 
  * ```ts
- * import { TbAccessibleFilled } from "react-icons/tb"
+ * import { TbAccessibleFilled } from "@preact-icons/tb"
  * ```
  * 
  * ## import a single icon, downloading just one icon
  * 
  * ```ts
- * import { TbAccessibleFilled } from "react-icons/tb/TbAccessibleFilled.ts"
+ * import { TbAccessibleFilled } from "react-icons/tb/TbAccessibleFilled"
  * ```
  * 
  * or using default export
