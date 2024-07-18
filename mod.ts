350408e6e9ea73d0c2bcfd6284ef3064c9b50866
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:41 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/tb)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/tb
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/tb
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Tabler Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/tb)](https://jsr.io/@preact-icons/tb)
- * 
- * **License** [MIT](https://opensource.org/licenses/MIT)
- * 
- * **Project** [https://github.com/tabler/tabler-icons](https://github.com/tabler/tabler-icons)
- * 
- * [See available icons here](https://react-icons.deno.dev/tb)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/tb
- * dnpx jsr add @preact-icons/tb
- * pnpm dlx jsr add @preact-icons/tb
- * bunx jsr add @preact-icons/tb
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import TbAccessibleFilled from "react-icons/tb/TbAccessibleFilled.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
